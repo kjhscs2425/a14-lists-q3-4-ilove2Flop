65447bd0c3ea1dbf584b31dfa1ef6504018dff3c
floppa <jbarek2027@gmail.com>
2025-03-19 19:19:15 -0700

    Done

--- q3.py
+++ q3.py
@@ -1,7 +1,7 @@
 def average(numbers):
     "Return the average of a list of numbers"
     # YOUR CODE HERE
-    return
+    return sum(numbers) / len(numbers)
 
 print(average([32, 78, 48, 71, 93, 71, 79, 44, 5, 42])) #56.3
 print(average([5, 4, 3, 2, 1])) # 3.0
--- q4.py
+++ q4.py
@@ -1,9 +1,12 @@
 def make_list_of_files(n):
     """
-    make a lists of n file names with the format "file_0.txt"
-    assume n greater than or equal to 1
-    hint: use a loop and append
+    Make a list of n file names with the format "file_0.txt"
+    Assume n is greater than or equal to 1
+    Hint: use a loop and append
     """
-    return
+    file_list = []
+    for i in range(n):
+        file_list.append(f"file_{i}.txt")
+    return file_list
 
-print(make_list_of_files(5)) #['file_0.txt', 'file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt']
+print(make_list_of_files(5))  # ['file_0.txt', 'file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt']
